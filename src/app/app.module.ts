50e9bca7675abcd768103e24569074b31996325d
Chandrashekhar Dhakad <chandrashekhar.dhakad@hotwaxsystems.com>
2018-06-13 22:48:33 +0530

    updated html file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule } from '@angular/material';
@@ -11,6 +12,8 @@ import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule } from 
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatButtonModule,
